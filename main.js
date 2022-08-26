e807215733579d0e907ef7a0fb011bacbacc7d1d
tristyn <trisclassie@gmaiil.com>
2022-08-26 14:43:44 -0600

    commit tree 8468ec95b2dd54c801a806f3f09b124455650070

--- main.js
+++ main.js
@@ -3,6 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  likedPost()
+} )
+
+
+const errorModal = document.querySelector("#modal")
+errorModal.classList.add("hidden")
+
+
+function likedPost() {
+  document.addEventListener("click", (event) => {
+    if(event.target.classList[0] === 'like-glyph') {
+    mimicServerCall()
+      .then((resp) => { 
+          const activated = event.target.classList.contains("activated-heart")
+          if(activated){ 
+          event.target.classList.remove("activated-heart")
+          event.target.innerHTML = EMPTY_HEART
+      }else{
+          event.target.classList.add("activated-heart")
+          event.target.innerHTML = FULL_HEART
+      }
+
+      })
+      .catch((error) => 
+        errorModal.classList.remove("hidden"))
+        setTimeout(() => errorModal.classList.add("hidden"), 3000)
+    }
+  })
+}
 
 
 
